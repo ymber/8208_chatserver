af05e0dbfe3d49366d3a232c3b242ebf9adff889
Matthew Taylor <m.r.taylor2@newcastle.ac.uk>
2024-03-18 13:18:00 +0000

    Load user keys on client startup

--- testing/client.py
+++ testing/client.py
@@ -5,6 +5,8 @@ import select
 import sys
 import argparse
 
+from cryptography.hazmat.primitives import serialization
+
 args = argparse.ArgumentParser(description="server")
 args.add_argument("addr", action="store", help="ip address")
 args.add_argument("port", type=int, action="store", help="port")
@@ -14,10 +16,21 @@ args_dict = vars(args.parse_args())
 class Client:
 
     def __init__(self):
+        print("Enter user id")
+        user_id = sys.stdin.readline().strip()
+        print("Enter private key password")
+        keypass = sys.stdin.readline().strip()
+        with open(f"{user_id}_private.pem", "rb") as keyfile:
+            self.private_key = serialization.load_pem_private_key(
+                keyfile.read(), password=keypass.encode())
+
+        with open(f"{user_id}_public.pem", "rb") as keyfile:
+            self.public_key = serialization.load_pem_public_key(keyfile.read())
+
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.connect((args_dict["addr"], args_dict["port"]))
         if self.server.recv(256) == b"Send user_id":
-            self.server.send(b"id_string")
+            self.server.send(user_id.encode())
 
     def execute(self):
         while True:
