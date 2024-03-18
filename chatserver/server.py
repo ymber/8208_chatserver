1e2bf0d8e762eaf31d31c2cb6a10dc7245e36ddd
Matthew Taylor <m.r.taylor2@newcastle.ac.uk>
2024-03-18 14:21:00 +0000

    Authenticate users by public key challenge

--- chatserver/server.py
+++ chatserver/server.py
@@ -3,6 +3,12 @@
 import socket
 import argparse
 import concurrent.futures
+import hashlib
+import os
+
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import serialization
 
 args = argparse.ArgumentParser(description="server")
 args.add_argument("addr", action="store", help="ip address")
@@ -14,6 +20,7 @@ class Server:
 
     def __init__(self):
         self.clients = {}
+        self.user_keys = {}
 
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -24,6 +31,26 @@ class Server:
         conn.send(b"Send user_id")
         user_id = conn.recv(256)
         return user_id.decode()
+
+    def authenticate_user(self, conn, user_id):
+        conn.send(b"Send public key")
+        public_key_bytes = conn.recv(4096)
+        if hashlib.sha256(public_key_bytes).hexdigest() != user_id:
+            conn.send(b"Public key does not match user id")
+
+        public_key = serialization.load_pem_public_key(public_key_bytes)
+        challenge = hashlib.sha256(os.urandom(1024)).hexdigest().encode()
+        ciphertext = public_key.encrypt(
+            challenge,
+            padding.OAEP(padding.MGF1(algorithm=hashes.SHA256()),
+                         hashes.SHA256(), None))
+        conn.send(ciphertext)
+        if conn.recv(256) == challenge:
+            conn.send(b"User authenticated")
+            return public_key
+        else:
+            conn.send(b"Authentication challenge failed")
+            return None
 
     def broadcast(self, user_id, msg):
         for uid in self.clients:
@@ -55,10 +82,14 @@ class Server:
             while True:
                 conn, addr = self.sock.accept()
                 user_id = self.identify_client(conn)
+                public_key = self.authenticate_user(conn, user_id)
                 self.clients[user_id] = conn
+                self.user_keys[user_id] = public_key
                 print(f"{addr[0]} connected with id {user_id}")
                 futures.append(
-                    executor.submit(self.client_handler, user_id=user_id, addr=addr))
+                    executor.submit(self.client_handler,
+                                    user_id=user_id,
+                                    addr=addr))
 
 
 if __name__ == "__main__":
--- testing/client.py
+++ testing/client.py
@@ -6,6 +6,8 @@ import sys
 import argparse
 
 from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
 
 args = argparse.ArgumentParser(description="server")
 args.add_argument("addr", action="store", help="ip address")
@@ -29,8 +31,31 @@ class Client:
 
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.connect((args_dict["addr"], args_dict["port"]))
+
         if self.server.recv(256) == b"Send user_id":
             self.server.send(user_id.encode())
+        else:
+            print("Client registration protocol violation. Exiting.")
+            sys.exit()
+        if self.server.recv(256) == b"Send public key":
+            self.server.send(
+                self.public_key.public_bytes(
+                    serialization.Encoding.PEM,
+                    serialization.PublicFormat.SubjectPublicKeyInfo))
+        else:
+            print("Client registration protocol violation. Exiting.")
+            sys.exit()
+
+        challenge_ciphertext = self.server.recv(4096)
+        challenge = self.private_key.decrypt(
+            challenge_ciphertext,
+            padding.OAEP(padding.MGF1(hashes.SHA256()), hashes.SHA256(), None))
+        self.server.send(challenge)
+        if self.server.recv(256) == b"User authenticated":
+            print("Authenticated")
+        else:
+            print("Client registration protocol violation. Exiting.")
+            sys.exit()
 
     def execute(self):
         while True:
